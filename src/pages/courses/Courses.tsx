51227f311c286b3535f3d73305d169097924a6a4
braiinybear <braiinybear@gmail.com>
2026-02-06 11:55:40 +0530

    ui fixes in course boxes

--- src/pages/courses/Courses.tsx
+++ src/pages/courses/Courses.tsx
@@ -179,7 +179,7 @@ console.log(courseCategoriesWise);
 
                     {/* Scrollable container if more than 5 courses */}
                     <div
-                      className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 scrollbar-hide"
+                      className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 scrollbar-hide"
                       style={{
                         maxHeight: "30rem",
                         overflowY: filteredCoursesInCategory.length > 5 ? "auto" : "visible",
